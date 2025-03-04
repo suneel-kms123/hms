cb9c068e6bc135e09a438458a7ed5e2a3eeceaea
suneel.kms123 <suneel.,kms123@gmail.com>
2025-03-04 13:17:41 +0530

    build success

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,9 @@
 FROM openjdk:17-jdk-alpine
 MAINTAINER suneel-kms123
+WORKDIR /app
+# Add the service's jar file
+COPY build/libs/*SNAPSHOT.jar /app.jar
+
+EXPOSE 8080
+# Run the service
+CMD ["java", "-jar", "app.jar"]
--- src/main/java/com/hms/hms/config/PostgresDataSourceConfiguration.java
+++ src/main/java/com/hms/hms/config/PostgresDataSourceConfiguration.java
@@ -1,18 +1,32 @@
 package com.hms.hms.config;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 import javax.sql.DataSource;
 
 @Configuration
 public class PostgresDataSourceConfiguration {
 
+    @Autowired
+    DataSourceProperties dataSourceProperties;
+
     @Bean
-    @ConfigurationProperties("spring.datasource")
+    @Primary
     public DataSource dataSource() {
-        return DataSourceBuilder.create().build();
+
+        HikariConfig hikariConfig=new HikariConfig();
+        hikariConfig.setJdbcUrl(dataSourceProperties.getUrl());
+        hikariConfig.setUsername(dataSourceProperties.getUsername());
+        hikariConfig.setPassword(dataSourceProperties.getPassword());
+        hikariConfig.setDriverClassName(dataSourceProperties.getDriverClassName());
+        return new HikariDataSource(hikariConfig);
     }
 }
